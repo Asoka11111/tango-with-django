2109ff2cafd9fab86e412c6cd3afa8627b59a52d
Ugonna Erinne <ue24@cornell.edu>
2016-06-14 12:20:53 -0500

    Got the register template to work

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.models import User
+from django.contrib.auth.models import User
 from rango.models import Page, Category, UserProfile
 
 class CategoryForm(forms.ModelForm):
@@ -38,14 +38,14 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
         return cleaned_data
 
-class UserForm(form.ModelForm):
+class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
 
-class UserProfileForm(form.ModelForm):
+class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('website', 'picture')
